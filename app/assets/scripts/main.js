04e218d4657d6216d373772bafc2d13791c6d952
Daniel da Silva <daniel@developmentseed.org>
2017-07-07 17:42:40 +0100

    Add interactivity

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,5 +1,28 @@
 'use strict';
-import config from './config';
+// Waypoints is global unfortunately...
+require('./vendor/waypoints');
 
-console.log.apply(console, config.consoleMessage);
-console.log('Environment', config.environment);
+const scrollContainer = document.querySelector('main.page__body');
+const menu = document.querySelectorAll('[data-hook="nav-menu"] a');
+
+menu.forEach(node => {
+  const target = document.querySelector(node.getAttribute('href'));
+  const activate = () => {
+    menu.forEach(n => n.classList.remove('ppn__menu-item--active'));
+    node.classList.add('ppn__menu-item--active');
+  };
+
+  node.addEventListener('click', e => {
+    e.preventDefault();
+    activate();
+    scrollContainer.scrollTop = scrollContainer.scrollTop + target.getBoundingClientRect().top - 16;
+  });
+
+  new Waypoint({
+    element: target,
+    context: scrollContainer,
+    handler: function () {
+      activate();
+    }
+  });
+});
